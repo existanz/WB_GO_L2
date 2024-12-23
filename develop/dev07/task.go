8fc55427d0a0ce592d81f4df973f2d70d42112a9
EXisTAnZ <ozim_ssspam@mail.ru>
2024-12-23 13:40:15 +0300

    solve: dev task 07

--- develop/dev07/task.go
+++ develop/dev07/task.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 /*
 === Or channel ===
 
@@ -35,4 +40,34 @@ fmt.Printf(“fone after %v”, time.Since(start))
 
 func main() {
 
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+
+	fmt.Printf("done after %v", time.Since(start))
+}
+
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	out := make(chan interface{})
+	for _, ch := range channels {
+		go func() {
+			<-ch
+			close(out)
+		}()
+	}
+	return out
 }
