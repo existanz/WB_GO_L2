178c2748427f7348c61a77a0ed325275dd5079df
EXisTAnZ <ozim_ssspam@mail.ru>
2024-12-28 17:29:24 +0300

    solve: dev task 08

--- develop/dev08/task.go
+++ develop/dev08/task.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
 /*
 === Взаимодействие с ОС ===
 
@@ -15,5 +25,90 @@ package main
 */
 
 func main() {
+	sh := NewShell(os.Stdout, os.Stdin)
+	if err := sh.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
 
+type Shell struct {
+	prompt []byte
+	w      io.Writer
+	r      io.Reader
+}
+
+func NewShell(w io.Writer, r io.Reader) *Shell {
+	return &Shell{
+		prompt: []byte("\033[32mmysh>\033[0m "),
+		w:      w,
+		r:      r,
+	}
+}
+
+func (sh *Shell) Run() error {
+	for {
+		sh.w.Write(sh.prompt)
+		input, err := bufio.NewReader(sh.r).ReadString('\n')
+		if err != nil {
+			sh.writeln("Error reading input:", err)
+			continue
+		}
+
+		commands := strings.Split(input, "|")
+		var processes []*exec.Cmd
+
+		for _, command := range commands {
+			args := strings.Fields(command)
+			if len(args) == 0 {
+				continue
+			}
+			if args[0] == "q" || args[0] == "quit" || args[0] == "e" || args[0] == "exit" {
+				os.Exit(0)
+			}
+			if args[0] == "cd" {
+				if len(args) < 2 {
+					sh.writeln("cd: missing argument")
+				} else {
+					if err := os.Chdir(args[1]); err != nil {
+						sh.writeln("cd:", err)
+					}
+				}
+				continue
+			}
+			cmd := exec.Command(args[0], args[1:]...)
+			if len(processes) > 0 {
+				cmd.Stdin, _ = processes[len(processes)-1].StdoutPipe()
+			}
+			processes = append(processes, cmd)
+		}
+
+		sh.executeCommands(processes)
+	}
+}
+
+func (sh *Shell) executeCommands(commands []*exec.Cmd) {
+	if len(commands) == 0 {
+		return
+	}
+
+	commands[len(commands)-1].Stdout = sh.w
+	commands[len(commands)-1].Stderr = sh.w
+
+	for _, cmd := range commands {
+		if err := cmd.Start(); err != nil {
+			sh.writeln("Error starting command:", err)
+			break
+		}
+	}
+
+	for _, cmd := range commands {
+		if err := cmd.Wait(); err != nil {
+			sh.writeln("Error waiting for command:", err)
+		}
+	}
+}
+
+func (sh *Shell) writeln(output ...any) {
+	line := fmt.Sprint(output...)
+	sh.w.Write([]byte(line + "\n"))
 }
